a32461a3ce01a50bd91485e652758b0333592c91
elounimedamine <74010738+elounimedamine@users.noreply.github.com>
2022-06-17 05:35:07 +0100

    Second commit

---
+++ lib/data/datasource/static/static.dart
@@ -0,0 +1,9 @@
+import 'package:ecommerce_app_project/core/constant/imageasset.dart';
+import 'package:ecommerce_app_project/data/model/onboarding.dart';
+
+List<OnBoardingModel> onBoardingList = [
+  OnBoardingModel(title: "Choose Product", body: "We have a 100k+ Products. Choose \n Your product from our E- \n commerce shop.", image: AppImageAsset.onBoardingImageOne),
+  OnBoardingModel(title: "Easy & Safe Payment", body:  "Easy Checkout & Safe Payment \n method. Trusted by our Customers \n from all over the world.", image: AppImageAsset.onBoardingImageTwo),
+  OnBoardingModel(title: "Track Your Order", body: "Best Tracker has been Used For \n Track your order. You'll know where \n your product is at the moment.", image: AppImageAsset.onBoardingImageThree),
+  OnBoardingModel(title: "Fast Delivery", body: "Reliable And Fast Delivery. We \n Deliver your product the fastest \n way possible.", image: AppImageAsset.onBoardingImageFour),
+];
---
+++ lib/data/model/onboarding.dart
@@ -0,0 +1,9 @@
+//concernant l'interface de début de l'application(bienvenue...) 
+
+class OnBoardingModel {
+  final String? title;
+  final String? image;
+  final String? body;
+
+  OnBoardingModel({this.body, this.title, this.image});
+}
